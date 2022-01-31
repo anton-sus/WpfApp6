a3757875aaeb9adc89c799dd0469f7fba83927dc
Anton S <geodesist.sushkov@gmail.com>
2022-01-31 23:45:28 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+//1.Разработать в WPF приложении класс WeatherControl, моделирующий погодную сводку – температуру (целое число в диапазоне от -50 до +50),
+//    направление ветра(строка), скорость ветра(целое число), наличие осадков(возможные значения: 0 – солнечно, 1 – облачно, 2 – дождь, 3 – снег.
+//    Можно использовать целочисленное значение,
+//    либо создать перечисление enum). Свойство «температура» преобразовать в свойство зависимости.
+namespace WpfApp6
+{
+    public partial class MainWindow: Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            WeatherControl weatherControl = new WeatherControl("NW", 25, Precipitation.cloudy);
+        }
+    }
+
+
+}
---
+++ WpfApp6/WeatherControl.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp6
+{
+    enum Precipitation
+    {
+        sunny,
+        cloudy,
+        rain,
+        snow
+    }
+    class WeatherControl : DependencyObject
+    {
+        private Precipitation precipitation;
+        private string wind_direction;
+        private int wind_speed;
+        public string WindDirection { get; set; }
+        public int WindSpeed { get; set; }
+        public WeatherControl(string wind, int windsp, Precipitation precipitation)
+        {
+            this.WindDirection = wind;
+            this.WindSpeed = windsp;
+            this.precipitation = precipitation;
+        }
+
+        public static readonly DependencyProperty TemperatureProperty;
+        public int Temperature
+        {
+            get => (int)GetValue(TemperatureProperty);
+            set => SetValue(TemperatureProperty, value);
+        }
+
+
+        static WeatherControl()
+        {
+            TemperatureProperty = DependencyProperty.Register(
+                nameof(Temperature),
+                typeof(int),
+                typeof(WeatherControl),
+                new FrameworkPropertyMetadata(
+                    0,
+                    FrameworkPropertyMetadataOptions.AffectsRender |
+                    FrameworkPropertyMetadataOptions.AffectsMeasure,
+                    null,
+                    new CoerceValueCallback(CoerceTemperature)),
+                new ValidateValueCallback(ValidateTemperature));
+        }
+
+        private static bool ValidateTemperature(object value)
+        {
+            int v = (int)value;
+            if (v >= -50 && v <= 50)
+                return true;
+            else
+                return false;
+        }
+
+        private static object CoerceTemperature(DependencyObject d, object baseValue)
+        {
+            int v = (int)baseValue;
+            if (v >= -50)
+                return v;
+            else
+                return 0;
+        }
+    }
+}
